951f7fa6a8f4a954ccc83c174b322927d9b485a9
tiomadre <91559090+tiomadre@users.noreply.github.com>
2025-10-26 00:40:23 -0400

    Reset mod to Farmer's Assortment baseline

---
+++ src/main/java/com/doltandtio/farmersassortment/FarmersAssortment.java
@@ -0,0 +1,15 @@
+package com.doltandtio.farmersassortment;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(FarmersAssortment.MOD_ID)
+public class FarmersAssortment {
+    public static final String MOD_ID = "farmersassortment";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public FarmersAssortment() {
+        LOGGER.info("Loading Farmer's Assortment");
+    }
+}
--- src/main/java/com/doltandtio/foragersinsight/core/mixin/MobEffectInstantMixin.java
+++
@@ -1,8 +0,0 @@
-package com.doltandtio.foragersinsight.core.mixin;
-
-import net.minecraft.world.effect.MobEffectInstance;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MobEffectInstance.class)
-public class MobEffectInstantMixin {
-}
