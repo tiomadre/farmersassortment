69c3cfc7f1945939da3a00d7f0970ac6265558a2
tiomadre <91559090+tiomadre@users.noreply.github.com>
2025-10-27 11:50:56 -0400

    Add data generation for cutting board recipes

---
+++ src/main/java/com/tiomadre/farmersassortment/data/FADatagen.java
@@ -0,0 +1,22 @@
+package com.tiomadre.farmersassortment.data;
+
+import com.tiomadre.farmersassortment.core.FarmersAssortment;
+import com.tiomadre.farmersassortment.data.server.recipes.FACrafting;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = FarmersAssortment.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class FADatagen {
+    private FADatagen() {
+    }
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        if (event.includeServer()) {
+            generator.addProvider(true, new FACrafting(generator.getPackOutput()));
+        }
+    }
+}
