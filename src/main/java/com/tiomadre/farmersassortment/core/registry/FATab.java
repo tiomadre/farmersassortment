83e217dfd1bbe7d4003bed93c3f075ff55c41b60
tiomadre <bd.pr.96@gmail.com>
2025-10-28 23:44:48 -0400

    -

--- src/main/java/com/tiomadre/farmersassortment/core/registry/FATab.java
+++ src/main/java/com/tiomadre/farmersassortment/core/registry/FATab.java
@@ -4,13 +4,16 @@ import com.tiomadre.farmersassortment.core.FarmersAssortment;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public final class FATab {
     public static final DeferredRegister<CreativeModeTab> TABS =
@@ -20,10 +23,15 @@ public final class FATab {
             .title(Component.translatable("itemGroup.farmersassortment"))
             .icon(() -> new ItemStack(FABlocks.SPRUCE_CUTTING_BOARD.get()))
             .displayItems((parameters, output) ->
-                    FAItems.ITEMS.getEntries().stream()
-                            .sorted(Comparator.comparing(registryObject -> registryObject.getId().getPath()))
-                            .map(RegistryObject::get)
-                            .map(Item::getDefaultInstance)
+                    Stream.concat(
+                                    FABlocks.BLOCKS.getEntries().stream()
+                                            .map(registryObject -> Map.entry(registryObject.getId(), (Supplier<? extends ItemLike>) registryObject)),
+                                    FAItems.ITEMS.getEntries().stream()
+                                            .map(registryObject -> Map.entry(registryObject.getId(), (Supplier<? extends ItemLike>) registryObject)))
+                            .sorted(Comparator.comparing(entry -> entry.getKey().getPath()))
+                            .map(Map.Entry::getValue)
+                            .map(Supplier::get)
+                            .map(ItemStack::new)
                             .forEach(output::accept))
             .build());
 
