610f286e4dbba2fac54ccfe3a48a8a12ba07ad3d
tiomadre <91559090+tiomadre@users.noreply.github.com>
2025-10-31 15:26:02 -0400

    Add Copper Cooking Pot GUI title

---
+++ src/main/java/com/tiomadre/farmersassortment/core/mixin/CookingPotBlockEntityMixin.java
@@ -0,0 +1,28 @@
+package com.tiomadre.farmersassortment.core.mixin;
+
+import com.tiomadre.farmersassortment.core.registry.FABlocks;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import vectorwing.farmersdelight.common.block.entity.CookingPotBlockEntity;
+
+@Mixin(CookingPotBlockEntity.class)
+public abstract class CookingPotBlockEntityMixin {
+
+    @Inject(method = "getName", at = @At("HEAD"), cancellable = true)
+    private void farmersassortment$overrideCopperCookingPotName(CallbackInfoReturnable<Component> cir) {
+        CookingPotBlockEntity self = (CookingPotBlockEntity) (Object) this;
+
+        if (self.getCustomName() != null) {
+            return;
+        }
+
+        BlockState state = self.getBlockState();
+        if (state.getBlock() == FABlocks.COPPER_COOKING_POT.get()) {
+            cir.setReturnValue(Component.translatable("container.farmersassortment.copper_cooking_pot"));
+        }
+    }
+}
