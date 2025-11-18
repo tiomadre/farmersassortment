c20dfb181e8fa76c0939756d6e0188541cf2be3d
tiomadre <bd.pr.96@gmail.com>
2025-11-18 14:38:27 -0500

    -

--- src/main/java/com/tiomadre/farmersassortment/data/server/recipes/FACrafting.java
+++ src/main/java/com/tiomadre/farmersassortment/data/server/recipes/FACrafting.java
@@ -8,6 +8,7 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeSerializer;
@@ -20,6 +21,7 @@ import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 import vectorwing.farmersdelight.common.block.CookingPotBlock;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public final class FACrafting extends RecipeProvider {
@@ -42,6 +44,17 @@ public final class FACrafting extends RecipeProvider {
         cuttingBoard(output, FABlocks.BAMBOO_CUTTING_BOARD, Blocks.BAMBOO_PLANKS);
         cuttingBoard(output, FABlocks.CRIMSON_CUTTING_BOARD, Blocks.CRIMSON_PLANKS);
         cuttingBoard(output, FABlocks.WARPED_CUTTING_BOARD, Blocks.WARPED_PLANKS);
+        butcherBlockCabinet(output, FABlocks.OAK_BUTCHER_BLOCK_CABINET, blockItem("farmersdelight", "cutting_board"), blockItem("farmersdelight", "oak_cabinet"));
+        butcherBlockCabinet(output, FABlocks.SPRUCE_BUTCHER_BLOCK_CABINET, FABlocks.SPRUCE_CUTTING_BOARD.get(), blockItem("farmersdelight", "spruce_cabinet"));
+        butcherBlockCabinet(output, FABlocks.BIRCH_BUTCHER_BLOCK_CABINET, FABlocks.BIRCH_CUTTING_BOARD.get(), blockItem("farmersdelight", "birch_cabinet"));
+        butcherBlockCabinet(output, FABlocks.JUNGLE_BUTCHER_BLOCK_CABINET, FABlocks.JUNGLE_CUTTING_BOARD.get(), blockItem("farmersdelight", "jungle_cabinet"));
+        butcherBlockCabinet(output, FABlocks.ACACIA_BUTCHER_BLOCK_CABINET, FABlocks.ACACIA_CUTTING_BOARD.get(), blockItem("farmersdelight", "acacia_cabinet"));
+        butcherBlockCabinet(output, FABlocks.DARK_OAK_BUTCHER_BLOCK_CABINET, FABlocks.DARK_OAK_CUTTING_BOARD.get(), blockItem("farmersdelight", "dark_oak_cabinet"));
+        butcherBlockCabinet(output, FABlocks.MANGROVE_BUTCHER_BLOCK_CABINET, FABlocks.MANGROVE_CUTTING_BOARD.get(), blockItem("farmersdelight", "mangrove_cabinet"));
+        butcherBlockCabinet(output, FABlocks.CHERRY_BUTCHER_BLOCK_CABINET, FABlocks.CHERRY_CUTTING_BOARD.get(), blockItem("farmersdelight", "cherry_cabinet"));
+        butcherBlockCabinet(output, FABlocks.BAMBOO_BUTCHER_BLOCK_CABINET, FABlocks.BAMBOO_CUTTING_BOARD.get(), blockItem("farmersdelight", "bamboo_cabinet"));
+        butcherBlockCabinet(output, FABlocks.CRIMSON_BUTCHER_BLOCK_CABINET, FABlocks.CRIMSON_CUTTING_BOARD.get(), blockItem("farmersdelight", "crimson_cabinet"));
+        butcherBlockCabinet(output, FABlocks.WARPED_BUTCHER_BLOCK_CABINET, FABlocks.WARPED_CUTTING_BOARD.get(), blockItem("farmersdelight", "warped_cabinet"));
         variantCookingPot(output, FABlocks.GOLDEN_COOKING_POT, Items.GOLD_INGOT);
         variantCookingPot(output, FABlocks.COPPER_COOKING_POT,Items.COPPER_INGOT);
         knife(output, FAItems.AMETHYST_KNIFE, Items.AMETHYST_SHARD);
@@ -80,6 +93,22 @@ public final class FACrafting extends RecipeProvider {
                 .unlockedBy(getHasName(material), has(material))
                 .save(output);
     }
+    private void butcherBlockCabinet(Consumer<FinishedRecipe> output, RegistryObject<? extends ItemLike> cabinet,
+                                     ItemLike cuttingBoard, ItemLike baseCabinet) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, cabinet.get())
+                .define('#', cuttingBoard)
+                .define('C', baseCabinet)
+                .pattern("#")
+                .pattern("C")
+                .unlockedBy(getHasName(cuttingBoard), has(cuttingBoard))
+                .save(output);
+    }
+
+    private ItemLike blockItem(String namespace, String path) {
+        ResourceLocation id = new ResourceLocation(namespace, path);
+        return Objects.requireNonNull(ForgeRegistries.BLOCKS.getValue(id), () -> "Missing block " + id);
+    }
+
 
     public static void register(IEventBus eventBus) {
         RECIPE_SERIALIZERS.register(eventBus);
