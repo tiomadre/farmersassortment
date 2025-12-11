3f746b58e46e23c80656cdd1f4c165e7085f90da
tiomadre <91559090+tiomadre@users.noreply.github.com>
2025-12-11 13:29:07 -0500

    Fix terracotta cooking pot item color models

--- src/main/java/com/tiomadre/farmersassortment/core/FAClientEvents.java
+++ src/main/java/com/tiomadre/farmersassortment/core/FAClientEvents.java
@@ -1,11 +1,17 @@
 package com.tiomadre.farmersassortment.core;
 
 import com.tiomadre.farmersassortment.client.renderer.ButcherBlockCabinetRenderer;
+import com.tiomadre.farmersassortment.core.FarmersAssortment;
+import com.tiomadre.farmersassortment.core.item.TerracottaCookingPotItem;
 import com.tiomadre.farmersassortment.core.registry.FABlockEntityTypes;
+import com.tiomadre.farmersassortment.core.registry.FAItems;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = FarmersAssortment.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class FAClientEvents {
@@ -15,5 +21,12 @@ public final class FAClientEvents {
     @SubscribeEvent
     public static void registerBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(FABlockEntityTypes.BUTCHER_BLOCK_CABINET.get(), ButcherBlockCabinetRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> ItemProperties.register(FAItems.TERRACOTTA_COOKING_POT.get(),
+                new ResourceLocation(FarmersAssortment.MOD_ID, "color"),
+                (stack, level, entity, seed) -> TerracottaCookingPotItem.getColor(stack).ordinal()));
     }
 }
